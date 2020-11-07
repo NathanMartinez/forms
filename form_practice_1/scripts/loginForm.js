c9f5137c2b2ee735227cf7e4dceac8e6b957c187
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-06 23:53:38 -0700

    added additional files for a later update

---
+++ form_practice_1/scripts/loginForm.js
@@ -0,0 +1,35 @@
+class LoginForm {
+  constructor() {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <h1>Login</h1>
+// <div class="social-media-div">
+//   <div class="social-icon-div"><i class="fab fa-facebook-f"></i></div>
+//   <div class="social-icon-div"><i class="fab fa-google"></i></div>
+//   <div class="social-icon-div"><i class="fab fa-linkedin-in"></i></div>
+// </div>
+// <p>or use your account</p>
+// <input type="text" name="username" id="username" placeholder="Username">
+// <input type="password" name="password" id="password" placeholder="Password">
+// <p><a href="#">Forgot Your Password</a></p>
+// <button id="login" type="button">LOGIN</button>
